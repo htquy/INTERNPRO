d336e6894b1a0286ac926816a30d0b243e1abbc6
htquy <hoangquydz0212@gmail.com>
2023-10-10 23:11:24 +0700

    Add project files.

---
+++ INTERNPRO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTERNPRO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
