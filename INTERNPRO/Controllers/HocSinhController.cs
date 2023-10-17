5e8958a8f5fd13e94f7ec8b84523d61cea445239
htquy <hoangquydz0212@gmail.com>
2023-10-17 17:10:49 +0700

    post modal box

--- INTERNPRO/Controllers/HocSinhController.cs
+++ INTERNPRO/Controllers/HocSinhController.cs
@@ -1,15 +1,53 @@
 ﻿using INTERNPRO.Datas;
+using INTERNPRO.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace INTERNPRO.Controllers
 {
     public class HocSinhController : Controller
-    {   private readonly InternProjectContext _db;
-        public HocSinhController(InternProjectContext db) 
+    {
+        private readonly InternProjectContext _db;
+        public HocSinhController(InternProjectContext db)
         { _db = db; }
+
         public IActionResult Index()
         {
-            return View(_db.HocSinhs);
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetHS()
+        {
+            List<HocSinh> hocsinhs = new List<HocSinh>();
+            hocsinhs = _db.HocSinhs.ToList();
+            ViewBag.HS = hocsinhs;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InsertHS(HocSinhModel hs)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var Student = new HocSinh()
+                {
+                    MaHs = hs.MaHs,
+                    PassWord = hs.PassWord,
+                    HoTenHs = hs.HoTenHs,
+                    QueQuan = hs.QueQuan,
+                    TenLop = hs.TenLop,
+                    GioiTinh = hs.GioiTinh,
+                    SoDienThoaiHs = hs.SoDienThoaiHs,
+                    SoDienThoaiPh = hs.SoDienThoaiPh,
+                    NgaySinh = hs.NgaySinh,
+                    HoTenPh = hs.HoTenPh
+                };
+
+                _db.HocSinhs.Add(Student);
+                _db.SaveChanges();
+                 return RedirectToAction("Index");
+             }
+                    return Json("Lỗi!!!");
+
         }
     }
 }
--- INTERNPRO/Datas/HocSinh.cs
+++ INTERNPRO/Datas/HocSinh.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Build.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace INTERNPRO.Datas;
@@ -6,22 +7,22 @@ namespace INTERNPRO.Datas;
 public partial class HocSinh
 {
     public int MaHs { get; set; }
-
-    public string PassWord { get; set; } = null!;
-
-    public string HoTenHs { get; set; } = null!;
-
-    public string QueQuan { get; set; } = null!;
-
-    public string TenLop { get; set; } = null!;
-
-    public string GioiTinh { get; set; } = null!;
+    [Required]
+    public string PassWord { get; set; }
+    [Required]
+    public string HoTenHs { get; set; }
+    [Required]
+    public string QueQuan { get; set; }
+    [Required]
+    public string TenLop { get; set; }
+    [Required]
+    public string GioiTinh { get; set; }
 
     public int SoDienThoaiHs { get; set; }
 
     public DateTime NgaySinh { get; set; }
-
-    public string HoTenPh { get; set; } = null!;
+    [Required]
+    public string HoTenPh { get; set; }
 
     public int? SoDienThoaiPh { get; set; }
 
