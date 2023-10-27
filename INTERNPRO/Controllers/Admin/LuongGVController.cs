ad3e45eecbcee9ac20aee0675a84246b426d1e01
htquy <hoangquydz0212@gmail.com>
2023-10-27 15:23:45 +0700

    no

--- INTERNPRO/Controllers/Admin/LuongGVController.cs
+++ INTERNPRO/Controllers/Admin/LuongGVController.cs
@@ -1,4 +1,5 @@
 ﻿using INTERNPRO.Datas;
+using INTERNPRO.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace INTERNPRO.Controllers.Admin
@@ -17,17 +18,38 @@ namespace INTERNPRO.Controllers.Admin
         [HttpGet]
         public IActionResult GetLuong() {
             var luong=_db.Luongs.ToList();
-            var gv=_db.GiaoViens.ToList();
-            var LuongGv = from L in luong
-                          join G in gv on L.MaLuong equals G.MaLuong
-                          select new
-                          {
-                              MaGV=G.MaGv,
-                              TenGV=G.TenGv,
-                              MaL=G.MaLuong,
-                              Luong=L.MucLuongCb
-                          };
-            return View(LuongGv);
+            var pcct =_db.PhanCongCts.ToList();
+            if (pcct.Count != 0)
+            {
+                
+                var gv = _db.GiaoViens.ToList();
+                var LuongGv = from L in luong
+                              join G in gv on L.MaLuong equals G.MaLuong
+                              orderby G.TenGv
+                              select new LuongModel
+                              {
+                                  MaGV = G.MaGv,
+                                  TenGV = G.TenGv,
+                                  MaL = G.MaLuong,
+                                  SoCa= pcct.Where(x=>x.MaGv.Equals(G.MaGv)).Count(),
+                                  Luong = L.MucLuongCb,
+                                  Thuong = G.ThuongThem,
+                              }
+                              ;
+                return View(LuongGv);
+            }
+            else return View();
         }
+        [HttpPost] 
+        public IActionResult PostLuong(List<int> dsthuong) {
+            var gv = _db.GiaoViens.OrderBy(x=>x.TenGv).ToList();
+            for(int i = 0; i < gv.Count(); i++)
+            {
+                gv[i].ThuongThem = dsthuong[i];
+            }
+            _db.SaveChanges();
+            return Json("post thành công");
+        }
+        
     }
 }
