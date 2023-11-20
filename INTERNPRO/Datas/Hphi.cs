57384ebf2df3bd0a49e73e7e3866282ea497806c
htquy <hoangquydz0212@gmail.com>
2023-11-20 14:18:29 +0700

    no prob

---
+++ INTERNPRO/Datas/Hphi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INTERNPRO.Datas;
+
+public partial class Hphi
+{
+    public string Ki { get; set; } = null!;
+
+    public int MaHs { get; set; }
+
+    public int Hpki { get; set; }
+
+    public int NoHpki { get; set; }
+
+    public int Hpthang { get; set; }
+
+    public int NoHpthang { get; set; }
+
+    public virtual HocSinh MaHsNavigation { get; set; } = null!;
+}
